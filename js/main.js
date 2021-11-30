fe134f2f76c20c7f20fc7c6f80ac2afaaf3ee3ce
Unclelake <enojarvimaria@gmail.com>
2021-11-30 16:20:37 +0100

    add event + bg img

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ function createItemHTML(item) {
   }
 
   
+searchInput.addEventListener("input", function () {
+    fetch("http://www.omdbapi.com/?apikey=9bb94a8c&s=" + searchInput.value)
+      .then((response) => {
+        return response.json();
+      })
+  
+      .then((data) => {
+        // reset
+        dataList.innerHTML = "";
+        if (data.Search) {
+        data.Search.forEach(function (item) {
+          dataList.innerHTML += createItemHTML(item);
+        });
+      }else {
+        dataList.innerHTML = "No Result"
+      }
+        
+      })
+  
+      .catch((error) => {
+        console.log(error);
+        dataList.innerHTML = "Oh no. Our server. It's broken";
+      });
+  });
